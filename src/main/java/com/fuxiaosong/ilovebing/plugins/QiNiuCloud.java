92fb6e2668a8de32dd43db512051710c6f61e942
xiaosongfu <xiaosong.fu@outlook.com>
2017-09-05 13:06:09 +0800

    添加判断逻辑

--- src/main/java/com/fuxiaosong/ilovebing/plugins/QiNiuCloud.java
+++ src/main/java/com/fuxiaosong/ilovebing/plugins/QiNiuCloud.java
@@ -17,10 +17,10 @@ import com.qiniu.util.Auth;
  */
 public class QiNiuCloud {
     //设置好账号的ACCESS_KEY和SECRET_KEY
-    private static final String ACCESS_KEY = "爱配不配";
-    private static final String SECRET_KEY = "爱配不配";
+    private static final String ACCESS_KEY = "";
+    private static final String SECRET_KEY = "";
     //要上传的空间
-    private static final String BUCKET_NAME = "爱配不配";
+    private static final String BUCKET_NAME = "";
 
     /**
      * 将文件上传到七牛云存储
@@ -29,32 +29,34 @@ public class QiNiuCloud {
      * @param fileName 上传到七牛后保存的文件名
      */
     public static void uploadToQiNiuCloud(String uploadFile, String fileName) {
-        //密钥配置
-        Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
+        if ("".equals(ACCESS_KEY) || "".equals(SECRET_KEY) || "".equals(BUCKET_NAME)) {
+            //密钥配置
+            Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
 
-        //第二种方式: 自动识别要上传的空间(bucket)的存储区域是华东、华北、华南。
-        Zone zone = Zone.autoZone();
-        Configuration configuration = new Configuration(zone);
+            //第二种方式: 自动识别要上传的空间(bucket)的存储区域是华东、华北、华南。
+            Zone zone = Zone.autoZone();
+            Configuration configuration = new Configuration(zone);
 
-        //创建上传对象
-        UploadManager uploadManager = new UploadManager(configuration);
+            //创建上传对象
+            UploadManager uploadManager = new UploadManager(configuration);
 
-        try {
-            // 获取 uploadToken
-            String uploadToken = auth.uploadToken(BUCKET_NAME);
-            //调用put方法上传
-            Response res = uploadManager.put(uploadFile, fileName, uploadToken);
-            //打印返回的信息
-            LogUtils.i("QiNiuCloud", res.bodyString());
-        } catch (QiniuException e) {
-            Response r = e.response;
-            // 请求失败时打印的异常的信息
-            LogUtils.i("QiNiuCloud", r.toString());
             try {
-                //响应的文本信息
-                LogUtils.i("QiNiuCloud", r.bodyString());
-            } catch (QiniuException e1) {
-                e1.printStackTrace();
+                // 获取 uploadToken
+                String uploadToken = auth.uploadToken(BUCKET_NAME);
+                //调用put方法上传
+                Response res = uploadManager.put(uploadFile, fileName, uploadToken);
+                //打印返回的信息
+                LogUtils.i("QiNiuCloud", res.bodyString());
+            } catch (QiniuException e) {
+                Response r = e.response;
+                // 请求失败时打印的异常的信息
+                LogUtils.i("QiNiuCloud", r.toString());
+                try {
+                    //响应的文本信息
+                    LogUtils.i("QiNiuCloud", r.bodyString());
+                } catch (QiniuException e1) {
+                    e1.printStackTrace();
+                }
             }
         }
     }
